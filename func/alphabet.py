74dfa1cd44891800860f022677ca310e0f8c3a57
Praveen <kumarspraveen57@gmail.com>
2025-06-02 01:08:07 +0530

    Delete func directory

--- func/alphabet.py
+++
@@ -1,9 +0,0 @@
-
-"""The alphabet."""
-
-letters = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-    "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-]
-
-LENGTH = len(letters)
